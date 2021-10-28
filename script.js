a038b57e3b81007649dee9f0211d8e9261077529
Smael Nicolas <nico.smael@gmail.com>
2021-10-28 15:08:53 -0300

    fix script, button calculate

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let date = "24 nov 2021";
+let date;
+const today = new Date();
 
 const dayEtiquette = document.getElementById("daysNumber");
 const hoursEtiquette = document.getElementById("hoursNumber");
 const minutesEtiquette = document.getElementById("minutesNumber");
 const secondsEtiquette = document.getElementById("secondsNumber");
 
+const dayInput = document.getElementById("inputDay");
+const monthInput = document.getElementById("inputMonth");
+const yearInput = document.getElementById("inputYear");
+
 const countDown = () => {
   const dateSelected = new Date(date);
-  const currentDate = new Date();
+  let currentDate = new Date();
 
   const totalTimeMiliseconds = dateSelected - currentDate;
   const totalTime = totalTimeMiliseconds / 1000;
@@ -27,5 +32,101 @@ const addHTML = (d, h, m, s) => {
   secondsEtiquette.innerHTML = s;
 };
 
-countDown();
-setInterval(countDown, 1000);
+const setInputs = () => {
+  let y = today.getFullYear();
+  let m = today.getMonth() + 1;
+  let d = today.getDate();
+
+  console.log(y, m, d);
+
+  dayInput.setAttribute("value", d);
+  monthInput.setAttribute("value", m);
+  yearInput.setAttribute("value", y);
+};
+
+const clickCalculate = () => {
+  let buttonCalculate = document.getElementById("calculate");
+
+  buttonCalculate.addEventListener("click", () => {
+    calculate();
+  });
+};
+
+const changeInputs = () => {
+  let inputGroup = document.getElementsByClassName("input");
+
+  for (let i = 0; i < inputGroup.length; i++) {
+    inputGroup[i].addEventListener("change", () => {
+      calculate();
+    });
+  }
+};
+
+const calculate = () => {
+  let month;
+
+  let d = dayInput.value;
+  let m = monthInput.value;
+  let y = yearInput.value;
+
+  if (
+    d < 1 ||
+    d > 31 ||
+    m < 1 ||
+    m > 12 ||
+    y < today.getFullYear() ||
+    isNaN(d) ||
+    isNaN(m) ||
+    isNaN(y)
+  ) {
+    alert("Insert a valid Date");
+  } else {
+    switch (m) {
+      case "1":
+        month = "jan";
+        break;
+      case "2":
+        month = "feb";
+        break;
+      case "3":
+        month = "mar";
+        break;
+      case "4":
+        month = "apr";
+        break;
+      case "5":
+        month = "may";
+        break;
+      case "6":
+        month = "jun";
+        break;
+      case "7":
+        month = "jul";
+        break;
+      case "8":
+        month = "aug";
+        break;
+      case "9":
+        month = "sep";
+        break;
+      case "10":
+        month = "oct";
+        break;
+      case "11":
+        month = "nov";
+        break;
+      case "12":
+        month = "dec";
+        break;
+    }
+
+    date = `${d} ${month} ${y}`;
+    console.log(date);
+    countDown();
+    setInterval(countDown, 1000);
+  }
+};
+
+clickCalculate();
+// changeInputs();
+setInputs();
