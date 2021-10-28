1d4323b0aee0ea211c4ed06e204bd02420dac190
Smael Nicolas <nico.smael@gmail.com>
2021-10-28 16:13:32 -0300

    check leap year

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const secondsEtiquette = document.getElementById("secondsNumber");
 const dayInput = document.getElementById("inputDay");
 const monthInput = document.getElementById("inputMonth");
 const yearInput = document.getElementById("inputYear");
+let inputGroup = document.getElementsByClassName("input");
 
 const countDown = () => {
   const dateSelected = new Date(date);
@@ -37,8 +38,6 @@ const setInputs = () => {
   let m = today.getMonth() + 1;
   let d = today.getDate();
 
-  console.log(y, m, d);
-
   dayInput.setAttribute("value", d);
   monthInput.setAttribute("value", m);
   yearInput.setAttribute("value", y);
@@ -46,7 +45,6 @@ const setInputs = () => {
 
 const clickCalculate = () => {
   let buttonCalculate = document.getElementById("calculate");
-  let inputGroup = document.getElementsByClassName("input");
 
   for (let i = 0; i < inputGroup.length; i++) {
     inputGroup[i].addEventListener("keyup", () => {
@@ -62,67 +60,90 @@ const clickCalculate = () => {
 };
 
 const calculate = () => {
-  let month;
-
   let d = dayInput.value;
   let m = monthInput.value;
   let y = yearInput.value;
 
-  if (
-    d < 1 ||
-    d > 31 ||
-    m < 1 ||
-    m > 12 ||
-    y < today.getFullYear() ||
-    isNaN(d) ||
-    isNaN(m) ||
-    isNaN(y)
-  ) {
-    alert("Insert a valid Date");
-  } else {
-    switch (m) {
-      case "1":
-        month = "jan";
-        break;
-      case "2":
-        month = "feb";
-        break;
-      case "3":
-        month = "mar";
-        break;
-      case "4":
-        month = "apr";
-        break;
-      case "5":
-        month = "may";
-        break;
-      case "6":
-        month = "jun";
-        break;
-      case "7":
-        month = "jul";
-        break;
-      case "8":
-        month = "aug";
-        break;
-      case "9":
-        month = "sep";
-        break;
-      case "10":
-        month = "oct";
-        break;
-      case "11":
-        month = "nov";
-        break;
-      case "12":
-        month = "dec";
-        break;
-    }
-
-    date = `${d} ${month} ${y}`;
+  if (isValid(d, m, y)) {
+    m = monthName(m);
+    date = `${d} ${m} ${y}`;
     countDown();
     setInterval(countDown, 1000);
+  } else {
+    alert("Insert a valid Date");
+    for (let i = 0; i < inputGroup.length; i++) {
+      inputGroup[i].value = "";
+    }
   }
+};
+
+const monthName = (m) => {
+  let month;
+  switch (m) {
+    case "1":
+      month = "jan";
+      break;
+    case "2":
+      month = "feb";
+      break;
+    case "3":
+      month = "mar";
+      break;
+    case "4":
+      month = "apr";
+      break;
+    case "5":
+      month = "may";
+      break;
+    case "6":
+      month = "jun";
+      break;
+    case "7":
+      month = "jul";
+      break;
+    case "8":
+      month = "aug";
+      break;
+    case "9":
+      month = "sep";
+      break;
+    case "10":
+      month = "oct";
+      break;
+    case "11":
+      month = "nov";
+      break;
+    case "12":
+      month = "dec";
+      break;
+  }
+  return month;
+};
+
+const isValid = (d, m, y) => {
+  let valid = true;
+
+  if (d < 1 || d > 31) {
+    valid = false;
+  }
+
+  if (m < 1 || m > 12) {
+    valid = false;
+  }
+
+  if (d == 31 && (m == 2 || m == 4 || m == 6 || m == 9 || m == 11)) {
+    valid = false;
+  }
+
+  if ((m == 2 && d == 29 && !(y % 4 == 0 && y % 100 != 0)) || y % 400 == 0) {
+    valid = false;
+  }
+
+  if (isNaN(d) || isNaN(m) || isNaN(y)) {
+    valid = false;
+  }
+
+  return valid;
 };
 
 clickCalculate();
