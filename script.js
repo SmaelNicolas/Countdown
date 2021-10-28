7eab3540f0ac4bd90ad026ed60dd7ea3f8db149a
Smael Nicolas <nico.smael@gmail.com>
2021-10-28 12:52:15 -0300

    styles

---
+++ script.js
@@ -0,0 +1,9 @@
+let newYear = "24 nov 2021";
+
+const countDown = () => {
+  const newYearsDate = new Date(newYear);
+  const currentDate = new Date();
+  console.log(currentDate);
+};
+
+countDown();
