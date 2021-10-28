cb1a6fe4c2f4664b90488e7af8c48d22ea795359
Smael Nicolas <nico.smael@gmail.com>
2021-10-28 13:05:00 -0300

    countdown functionality

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-let newYear = "24 nov 2021";
+let date = "24 nov 2021";
+
+const dayEtiquette = document.getElementById("daysNumber");
+const hoursEtiquette = document.getElementById("hoursNumber");
+const minutesEtiquette = document.getElementById("minutesNumber");
+const secondsEtiquette = document.getElementById("secondsNumber");
 
 const countDown = () => {
-  const newYearsDate = new Date(newYear);
+  const dateSelected = new Date(date);
   const currentDate = new Date();
-  console.log(currentDate);
+
+  const totalTimeMiliseconds = dateSelected - currentDate;
+  const totalTime = totalTimeMiliseconds / 1000;
+
+  const days = Math.round(totalTime / 3600 / 24);
+  const hours = Math.round(totalTime / 3600) % 24;
+  const minutes = Math.round(totalTime / 60) % 60;
+  const seconds = Math.round(totalTime % 60);
+
+  addHTML(days, hours, minutes, seconds);
+};
+
+const addHTML = (d, h, m, s) => {
+  dayEtiquette.innerHTML = d;
+  hoursEtiquette.innerHTML = h;
+  minutesEtiquette.innerHTML = m;
+  secondsEtiquette.innerHTML = s;
 };
 
 countDown();
+setInterval(countDown, 1000);
